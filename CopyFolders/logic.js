ae8b38301df0f30bcc025d4c7a0d5e71c54afc72
najee <najeeb111.111@yahoo.com>
2021-05-02 08:41:12 +0300

    twit

---
+++ CopyFolders/logic.js
@@ -0,0 +1,28 @@
+let array = [
+    {
+        text: "First post!",
+        id: "p1",
+        comments: [
+            { id: "c1", text: "First comment on first post!" },
+            { id: "c2", text: "Second comment on first post!!" },
+            { id: "c3", text: "Third comment on first post!!!" }
+        ]
+    },
+    {
+        text: "Aw man, I wanted to be first",
+        id: "p2",
+        comments: [
+            { id: "c4", text: "Don't wory second poster, you'll be first one day." },
+            { id: "c5", text: "Yeah, believe in yourself!" },
+            { id: "c6", text: "Haha second place what a joke." }
+        ]
+    }
+]
+
+let postIdCounter = array.length;
+console.log(postIdCounter)
+let commentIdCounter = 0
+for (let post of array){
+    commentIdCounter += post.comments.length
+}
+console.log(commentIdCounter)
